0ff59329ec461269c030ea014431d3a10b25ddcf
Jonathan Roley <jonathan.roley@gmail.com>
2022-06-23 15:25:26 -0400

    Changed player movement

--- server/player.js
+++ server/player.js
@@ -5,22 +5,32 @@ class Player {
     this.username = username
     this.x = x
     this.y = y
+    this.xVel = 0
+    this.yVel = 0
+    this.accel = 10
     this.angle = 0
     this.speed = 200
+    this.slowdown = 0.9
     this.moveState = {}
   }
   handleInput(input) {
     this.moveState = input
   }
   update(dt) {
-    let xVel = 0, yVel = 0
-    if(this.moveState.up) yVel -= 1
-    if(this.moveState.down) yVel += 1
-    if(this.moveState.left) xVel -= 1
-    if(this.moveState.right) xVel += 1
+    if(this.moveState.up) this.yVel -= this.accel
+    else if(this.moveState.down) this.yVel += this.accel
+    else this.yVel *= this.slowdown
+    
+    if(this.moveState.left) this.xVel -= this.accel
+    else if(this.moveState.right) this.xVel += this.accel
+    else this.xVel *= this.slowdown
+
+    this.xVel = Math.min(this.speed, Math.max(this.xVel, -this.speed))
+    this.yVel = Math.min(this.speed, Math.max(this.yVel, -this.speed))
+
     this.angle = this.moveState.angle
-    this.x += xVel * dt * this.speed
-    this.y += yVel * dt * this.speed
+    this.x += this.xVel * dt
+    this.y += this.yVel * dt
   }
   getData() {
     return {
